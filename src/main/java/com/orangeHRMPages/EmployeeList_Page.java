26c36a163a8e6ba743b473eb0bd40c618e35253d
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-04 16:27:50 +0530

    Update code

--- src/main/java/com/orangeHRMPages/EmployeeList_Page.java
+++ src/main/java/com/orangeHRMPages/EmployeeList_Page.java
@@ -11,16 +11,33 @@ public class EmployeeList_Page extends Base_Page
         super();
         PageFactory.initElements(driver,this);
     }
+
     @FindBy(xpath="//input[@placeholder='Type for hints...']")
     WebElement empName;
+
     @FindBy(xpath="/html/body/div/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input")
     WebElement empID;
+
     @FindBy(xpath="//button[@type='submit']")
     WebElement search;
+
+    @FindBy(xpath="//span[contains(normalize-space(),'Record Found')]")
+    WebElement recordFound;
+
     public void clickSearchButton()
     {
         writeText(properties.getProperty("empName"),empName);
-        //writeText(properties.getProperty("empID"),empID);
+        writeText(properties.getProperty("empID"),empID);
         clickButton(search);
+    }
+
+    public String actualOutput()
+    {
+        return recordFound.getText();
+    }
+
+    public String expectedOutput()
+    {
+        return "(1) Record Found";
     }
 }
