ef93274de8cfe45d6c523925b2af6c23155c193f
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-04 16:28:45 +0530

    Update code

--- src/main/java/com/orangeHRMPages/ForgotPassword_Page.java
+++ src/main/java/com/orangeHRMPages/ForgotPassword_Page.java
@@ -13,17 +13,33 @@ public class ForgotPassword_Page extends Base_Page
         super();
         PageFactory.initElements(driver,this);
     }
+
     @FindBy(xpath="//p[text()='Forgot your password? ']")
     WebElement forgotPassword;
+
     @FindBy(xpath="//input[@name='username']")
     WebElement userName;
+
     @FindBy(xpath="//button[@type='submit']")
     WebElement submit;
+
+    @FindBy(xpath="//h6[text()='Reset Password link sent successfully']")
+    WebElement verifyResetPassword;
 
     public void passwordReset()
     {
         clickButton(forgotPassword);
         writeText(properties.getProperty("userName"),userName);
         clickButton(submit);
+    }
+
+    public String actualOutput()
+    {
+        return verifyResetPassword.getText();
+    }
+
+    public String expectedOutput()
+    {
+        return "Reset Password link sent successfully";
     }
 }
