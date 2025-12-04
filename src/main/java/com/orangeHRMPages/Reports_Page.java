2e1ae36204355dc3d135415eafc501b2c56cca53
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-04 16:29:57 +0530

    Update code

--- src/main/java/com/orangeHRMPages/Reports_Page.java
+++ src/main/java/com/orangeHRMPages/Reports_Page.java
@@ -11,10 +11,13 @@ public class Reports_Page extends Base_Page
         super();
         PageFactory.initElements(driver,this);
     }
+
     @FindBy(xpath="//input[@placeholder='Type for hints...']")
     WebElement report;
+
     @FindBy(xpath="//button[@type='submit']")
     WebElement search;
+
     public void employeeReport()
     {
         writeText(properties.getProperty("reportName"),report);
