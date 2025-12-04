90ffb1bc686fd4e5352579ca22ba6cf3f6bb6a5c
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-04 16:44:32 +0530

    Update code

--- src/test/java/com/orangeHRMTests/PIMPageTest.java
+++ src/test/java/com/orangeHRMTests/PIMPageTest.java
@@ -4,6 +4,7 @@ import com.orangeHRMPages.Base_Page;
 import com.orangeHRMPages.Dashboard_Page;
 import com.orangeHRMPages.Login_Page;
 import com.orangeHRMPages.PIM_Page;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,17 +27,30 @@ public class PIMPageTest
         loginPage.login();
         dashboardPage.navigateToPIM();
     }
-    @Test
-    public void verify()
+    @Test(priority=1)
+    public void verifyAddEmployee()
     {
         pimPage.navigateToAddEmployee();
+        Assert.assertEquals(pimPage.actualAddEmployee(),pimPage.expectedAddEmployee(),"Couldn't Navigate to Add Employee Page");
+    }
+    @Test(priority=2)
+    public void verifyEmployeeList()
+    {
+        pimPage.navigateToEmployeeList();
+        Assert.assertEquals(pimPage.actualEmployeeList(),pimPage.expectedEmployeeList(),"Couldn't Navigate to Employee List Page");
+    }
+    @Test(priority=3)
+    public void verifyReport()
+    {
+        pimPage.navigateToReport();
+        Assert.assertEquals(pimPage.actualEmployeeReport(),pimPage.expectedEmployeeReport(),"Couldn't Navigate to Employee Reports Page");
     }
     @AfterMethod
     public void tearDown()
     {
         try
         {
-            Thread.sleep(15000);
+            Thread.sleep(10000);
             basePage.quit();
         }
         catch(InterruptedException e)
