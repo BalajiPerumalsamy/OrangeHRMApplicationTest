36a1df0d31a13c6b6b226244ddadf3e65a6fe8c9
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-04 16:30:46 +0530

    Update code

--- src/test/java/com/orangeHRMTests/AddEmployeePageTest.java
+++ src/test/java/com/orangeHRMTests/AddEmployeePageTest.java
@@ -14,7 +14,7 @@ public class AddEmployeePageTest
     public Login_Page loginPage;
     public Dashboard_Page dashboardPage;
     public PIM_Page pimPage;
-    public AddEmployee_page addEmployeePage;
+    public AddEmployee_Page addEmployeePage;
 
     @BeforeMethod
     public void setUp()
@@ -24,10 +24,11 @@ public class AddEmployeePageTest
         loginPage=new Login_Page();
         dashboardPage=new Dashboard_Page();
         pimPage=new PIM_Page();
-        addEmployeePage=new AddEmployee_page();
+        addEmployeePage=new AddEmployee_Page();
         loginPage.login();
         dashboardPage.navigateToPIM();
         pimPage.navigateToAddEmployee();
+        Assert.assertEquals(pimPage.actualAddEmployee(),pimPage.expectedAddEmployee(),"Couldn't navigate to Add Employee page");
     }
     @DataProvider(name="excelData")
     public Object[][] getEmployeeData()
@@ -40,15 +41,14 @@ public class AddEmployeePageTest
     public void verifyAddEmployee(String firstName, String lastName,String licenceNum,String licenceExpDate,String dofBirth)
     {
         addEmployeePage.AddEmployee(firstName,lastName,licenceNum,licenceExpDate,dofBirth);
+        Assert.assertEquals(addEmployeePage.actualDataSaved(),addEmployeePage.expectedDataSaved(),"Couldn't save user data");
+        Assert.assertEquals(addEmployeePage.actualDataUpdated(),addEmployeePage.expectedDataUpdated(),"Couldn't update user data");
     }
     @AfterMethod
     public void tearDown()
     {
         try
         {
-            Assert.assertEquals(pimPage.actualAddEmployee(),pimPage.expectedAddEmployee(),"Actual and Expected Output is mismatch");
-            Assert.assertEquals(addEmployeePage.actualDataSaved(),addEmployeePage.expectedDataSaved(),"Actual and Expected Output is mismatch");
-            Assert.assertEquals(addEmployeePage.actualDataUpdated(),addEmployeePage.expectedDataUpdated(),"Actual and Expected Output is mismatch");
             Thread.sleep(10000);
             basePage.quit();
         }
