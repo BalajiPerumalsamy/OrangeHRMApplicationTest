6aded9eba6a0e42cde361d1a00047f136c2cd790
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-11 19:06:51 +0530

    Add Screenshot method

--- src/test/java/com/orangeHRMTests/DeleteEmployeePageTest.java
+++ src/test/java/com/orangeHRMTests/DeleteEmployeePageTest.java
@@ -28,14 +28,31 @@ public class DeleteEmployeePageTest
         pimPage=new PIM_Page();
         pimPage.navigateToEmployeeList();
         employeeListPage=new EmployeeList_Page();
-        employeeListPage.clickSearchButton();
+        deleteEmployeePage=new DeleteEmployee_Page();
+        try
+        {
+            employeeListPage.clickSearchButton();
+            Assert.assertEquals(deleteEmployeePage.actualOutputRecord(),deleteEmployeePage.expectedOutputRecord(),"No employee record found");
+        }
+        catch(AssertionError e)
+        {
+            System.out.println("Error acquired Page not move forward");
+            basePage.captureScreenshot("EmployeeNoRecord");
+        }
     }
     @Test
     public void verifyDelete()
     {
-        deleteEmployeePage=new DeleteEmployee_Page();
-        deleteEmployeePage.delete();
-        Assert.assertEquals(deleteEmployeePage.actualOutput(),deleteEmployeePage.expectedOutput(),"Couldn't Delete user data");
+        try
+        {
+            deleteEmployeePage.delete();
+            Assert.assertEquals(deleteEmployeePage.actualOutput(),deleteEmployeePage.expectedOutput(),"Couldn't Delete user data");
+        }
+        catch(AssertionError e)
+        {
+            System.out.println("Error acquired Page not move forward");
+            basePage.captureScreenshot("DeletePage");
+        }
     }
     @AfterMethod
     public void tearDown()
