c414609e78780837b41c9c99ad160a4b30aa13b7
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-15 22:33:08 +0530

    Add Extent Reports

---
+++ src/test/java/com/reports/ReportManager.java
@@ -0,0 +1,22 @@
+package com.reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ReportManager
+{
+    public static ExtentReports extent;
+
+    public static ExtentReports getReport() {
+        ExtentSparkReporter reporter =
+                new ExtentSparkReporter("ExtentReport.html");
+
+        reporter.config().setReportName("Automation Test Report");
+        reporter.config().setDocumentTitle("OrangeHRM Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+
+        return extent;
+    }
+}
