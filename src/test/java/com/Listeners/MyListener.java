61307a5512932285ee4712794b259580119a9062
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-16 21:29:10 +0530

    Code updatd

--- src/test/java/com/Listeners/MyListener.java
+++ src/test/java/com/Listeners/MyListener.java
@@ -3,6 +3,7 @@ package com.Listeners;
 import com.aventstack.extentreports.*;
 import com.orangeHRMPages.Base_Page;
 import com.reports.ReportManager;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -12,20 +13,23 @@ public class MyListener implements ITestListener
     ExtentTest test;
     Base_Page basePage;
 
-    @Override
+    public void onStart(ITestContext context)
+    {
+        System.out.println("Test Suite Started : " + context.getName());
+        System.out.println("Test Suite Started time : "+context.getStartDate());
+    }
+
     public void onTestStart(ITestResult result)
     {
         test=extent.createTest(result.getName());
         test.log(Status.INFO,"Test Started");
     }
 
-    @Override
     public void onTestSuccess(ITestResult result)
     {
         test.log(Status.PASS,"Test Passed");
     }
 
-    @Override
     public void onTestFailure(ITestResult result)
     {
         String testName=result.getName();
@@ -35,23 +39,24 @@ public class MyListener implements ITestListener
         test.fail(result.getThrowable(),MediaEntityBuilder.createScreenCaptureFromPath(path).build());
     }
 
-    @Override
     public void onTestSkipped(ITestResult result)
     {
-        test=extent.createTest(result.getName());
-        test.skip("Test Skipped");
+        test.log(Status.SKIP,"Test Skipped");
         if(result.getThrowable()!=null)
         {
-            test.skip("Skip Reason: "+result.getThrowable().getMessage());
+            test.skip("Skip Reason : "+result.getThrowable().getMessage());
         }
         basePage=new Base_Page();
         String path=basePage.captureScreenshot(result.getName());
         test.skip(MediaEntityBuilder.createScreenCaptureFromPath(path).build());
     }
 
-    @Override
-    public void onFinish(org.testng.ITestContext context)
+    public void onFinish(ITestContext context)
     {
+        System.out.println("Total Passed Tests : " + context.getPassedTests().size());
+        System.out.println("Total Failed Tests : " + context.getFailedTests().size());
+        System.out.println("Total Skipped Tests : " + context.getSkippedTests().size());
+        System.out.println("Test Suite End Time : " + context.getEndDate());
         extent.flush();
     }
 }
