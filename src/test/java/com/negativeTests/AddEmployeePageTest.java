ce7cbec5023ae028ad3cc35b7ff6ae63e9435cc7
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-02-06 22:13:29 +0530

    New Test case

---
+++ src/test/java/com/negativeTests/AddEmployeePageTest.java
@@ -0,0 +1,64 @@
+package com.negativeTests;
+
+import com.Listeners.MyListener;
+import com.orangeHRMPages.*;
+import com.utils.ExcelUtils;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.Properties;
+
+@Listeners(MyListener.class)
+public class AddEmployeePageTest
+{
+    public Base_Page basePage;
+    public Login_Page loginPage;
+    public Dashboard_Page dashboardPage;
+    public PIM_Page pimPage;
+    public AddEmployee_Page addEmployeePage;
+    public Properties properties;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        basePage=new Base_Page();
+        basePage.navigateToApplication();
+        loginPage=new Login_Page();
+        dashboardPage=new Dashboard_Page();
+        pimPage=new PIM_Page();
+        addEmployeePage=new AddEmployee_Page();
+        properties=new Properties();
+        loginPage.login(basePage.properties.getProperty("userName"),basePage.properties.getProperty("password"));
+        dashboardPage.navigateToPIM();
+        pimPage.navigateToAddEmployee();
+        Assert.assertEquals(pimPage.actualAddEmployee(),pimPage.expectedAddEmployee(),"Couldn't navigate to Add Employee page");
+    }
+
+    @DataProvider(name="excelData")
+    public Object[][] getEmployeeData()
+    {
+        String filePath="C://Users//ELCOT//Desktop//Test case//InputData.xlsx";
+        return ExcelUtils.getExcelData(filePath, "Sheet1");
+    }
+
+    @Test(dataProvider="excelData")
+    public void verifyNegativeAddEmployeeData(String firstName,String lastName,String licenceNum,String licenceExpDate,String dofBirth)
+    {
+        addEmployeePage.employeeAdd(firstName);
+        Assert.assertEquals(addEmployeePage.invalidActualDataSaved(), addEmployeePage.invalidExpectedDataSaved(),"User data Saved Successfully Saved");
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        try
+        {
+            Thread.sleep(10000);
+            basePage.quit();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
