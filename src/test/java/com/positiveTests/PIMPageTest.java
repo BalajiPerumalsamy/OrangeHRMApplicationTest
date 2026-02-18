616f7e5b45cd3103595c6a98b320e0e646ccc514
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-02-18 21:47:11 +0530

    Code Modified

--- src/test/java/com/positiveTests/PIMPageTest.java
+++ src/test/java/com/positiveTests/PIMPageTest.java
@@ -30,7 +30,6 @@ public class PIMPageTest
         loginPage=new Login_Page();
         dashboardPage=new Dashboard_Page();
         pimPage=new PIM_Page();
-        properties=new Properties();
         loginPage.login(basePage.properties.getProperty("userName"),basePage.properties.getProperty("password"));
         dashboardPage.navigateToPIM();
     }
