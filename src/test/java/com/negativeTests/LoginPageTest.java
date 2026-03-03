bc5c1cb1fe3ae481f1b7ba5b33bf804bdbcbdee3
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-03 21:43:15 +0530

    Code Modified

--- src/test/java/com/negativeTests/LoginPageTest.java
+++ src/test/java/com/negativeTests/LoginPageTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import java.util.Properties;
 
 @Listeners(MyListener.class)
