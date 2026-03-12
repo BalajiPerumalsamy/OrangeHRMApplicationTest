fd93b57f9048179dcabff8f3925f3c7b9d69a93f
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-12 20:05:17 +0530

    Code Modified

--- src/test/java/com/negativeTests/ChangePasswordMisMatchPasswordTest.java
+++ src/test/java/com/negativeTests/ChangePasswordMisMatchPasswordTest.java
@@ -9,14 +9,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import java.util.Properties;
 
 @Listeners(MyListener.class)
 public class ChangePasswordMisMatchPasswordTest
 {
     public Base_Page basePage;
     public Login_Page loginPage;
-    public Properties properties;
     public ChangePassword_Page passwordPage;
 
     @BeforeMethod
