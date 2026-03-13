b46d560cd5982590a5dddeaa099237548f41a88c
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-13 20:59:50 +0530

    New Page Add

---
+++ src/main/java/com/orangeHRMPages/Buzz_Page.java
@@ -0,0 +1,29 @@
+package com.orangeHRMPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Buzz_Page extends Base_Page
+{
+    public Buzz_Page()
+    {
+        super();
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath="//textarea[@placeholder=\"What's on your mind?\"]")
+    WebElement content;
+
+    @FindBy(xpath="//button[@type='submit']")
+    WebElement clickPost;
+
+    @FindBy(xpath="//p[contains(@class,'oxd-text--toast-message')]")
+    public WebElement confirmationMessage;
+
+    public void buzzNewsFeed(String text)
+    {
+        writeText(text,content);
+        clickButton(clickPost);
+    }
+}
