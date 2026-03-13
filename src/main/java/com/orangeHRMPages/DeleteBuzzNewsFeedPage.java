dce51aa31ad986a5db74a8062d5db47ccf7d9383
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-13 21:00:12 +0530

    New Page Add

---
+++ src/main/java/com/orangeHRMPages/DeleteBuzzNewsFeedPage.java
@@ -0,0 +1,34 @@
+package com.orangeHRMPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DeleteBuzzNewsFeedPage extends Base_Page
+{
+    public DeleteBuzzNewsFeedPage()
+    {
+        super();
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath="//i[@class='oxd-icon bi-three-dots']")
+    WebElement icon;
+
+    @FindBy(xpath="//p[text()='Delete Post']")
+    WebElement delete;
+
+    @FindBy(xpath="//button[text()=' Yes, Delete ']")
+    WebElement clickDeleteButton;
+
+    @FindBy(xpath="//p[contains(@class,'oxd-text--toast-message')]")
+    WebElement message;
+
+    public void delete()
+    {
+        clickButton(icon);
+        clickButton(delete);
+        clickButton(clickDeleteButton);
+        System.out.println("Confirmation message = "+message.getText());
+    }
+
+}
