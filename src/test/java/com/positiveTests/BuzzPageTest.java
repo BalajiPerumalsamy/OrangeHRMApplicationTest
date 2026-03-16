a9fc69f4b70b5c1926f8bfabda723f70c02ff677
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:50:34 +0530

    Code Modified

--- src/test/java/com/positiveTests/BuzzPageTest.java
+++ src/test/java/com/positiveTests/BuzzPageTest.java
@@ -42,14 +42,6 @@ public class BuzzPageTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
