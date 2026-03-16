439166639d5b6f339d11a5ae5f8db648888e0cf2
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:46:40 +0530

    Code Modified

--- src/test/java/com/positiveTests/PIMPageTest.java
+++ src/test/java/com/positiveTests/PIMPageTest.java
@@ -56,14 +56,6 @@ public class PIMPageTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
