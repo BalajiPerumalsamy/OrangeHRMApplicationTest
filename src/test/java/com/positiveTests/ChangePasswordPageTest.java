4a816dd2b12fc8eff6bae295661cef26808007d1
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:49:49 +0530

    Code Modified

--- src/test/java/com/positiveTests/ChangePasswordPageTest.java
+++ src/test/java/com/positiveTests/ChangePasswordPageTest.java
@@ -39,14 +39,6 @@ public class ChangePasswordPageTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
