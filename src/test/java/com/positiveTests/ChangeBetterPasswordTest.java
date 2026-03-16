d3cad03e75a6cabc0b1918555b2cd7b5c113204c
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:50:06 +0530

    Code Modified

--- src/test/java/com/positiveTests/ChangeBetterPasswordTest.java
+++ src/test/java/com/positiveTests/ChangeBetterPasswordTest.java
@@ -39,14 +39,6 @@ public class ChangeBetterPasswordTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
