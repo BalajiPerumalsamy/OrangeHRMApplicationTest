7b51982043d3baf5ef23e924337cb0740bf68e8d
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:51:05 +0530

    Code Modified

--- src/test/java/com/negativeTests/LoginPageTest.java
+++ src/test/java/com/negativeTests/LoginPageTest.java
@@ -33,14 +33,6 @@ public class LoginPageTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
