08b5010db6e9576e77150d08a4515028ca47cea1
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-16 20:47:00 +0530

    Code Modified

--- src/test/java/com/positiveTests/LogoutPageTest.java
+++ src/test/java/com/positiveTests/LogoutPageTest.java
@@ -39,14 +39,6 @@ public class LogoutPageTest
     @AfterMethod
     public void tearDown()
     {
-        try
-        {
-            Thread.sleep(10000);
-            basePage.quit();
-        }
-        catch(InterruptedException e)
-        {
-            System.out.println(e);
-        }
+        basePage.quitApplication();
     }
 }
