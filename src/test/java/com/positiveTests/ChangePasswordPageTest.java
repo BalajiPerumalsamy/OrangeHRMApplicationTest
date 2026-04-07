f19830d2296babfe74043e475c3c3e6ed5d48385
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-07 19:56:45 +0530

    Code Modified

--- src/test/java/com/positiveTests/ChangePasswordPageTest.java
+++ src/test/java/com/positiveTests/ChangePasswordPageTest.java
@@ -10,7 +10,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-
 @Listeners(MyListener.class)
 public class ChangePasswordPageTest
 {
