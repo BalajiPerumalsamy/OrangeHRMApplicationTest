f49bee7073d0f0b4cf4516be11ccb613c6682143
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-15 13:22:38 +0530

    Code Modified

--- src/main/java/com/orangeHRMPages/EmployeeList_Page.java
+++ src/main/java/com/orangeHRMPages/EmployeeList_Page.java
@@ -1,11 +1,12 @@
 package com.orangeHRMPages;
 
+import com.BassPage.BaseClass;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class EmployeeList_Page extends Base_Page
+public class EmployeeList_Page extends BaseClass
 {
     public EmployeeList_Page()
     {
