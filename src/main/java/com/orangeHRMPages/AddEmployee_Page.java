3a7184e6b0d014e3126ed445cbddcc0045ec39b5
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-15 13:24:07 +0530

    Code Modified

--- src/main/java/com/orangeHRMPages/AddEmployee_Page.java
+++ src/main/java/com/orangeHRMPages/AddEmployee_Page.java
@@ -1,11 +1,12 @@
 package com.orangeHRMPages;
 
+import com.BassPage.BaseClass;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class AddEmployee_Page extends Base_Page
+public class AddEmployee_Page extends BaseClass
 {
     String firstMessage;
     String secondMessage;
